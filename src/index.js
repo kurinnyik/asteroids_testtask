53fadfef318b77c22b95d2253d55e8b92fc2c69c
Kostya <kurinniy.k@gmail.com>
2020-06-13 11:19:29 +0300

    asteroids hit added

--- src/index.js
+++ src/index.js
@@ -22,18 +22,27 @@ class Asteroid extends MovingObject {
   draw(ctx) {
     let {x, y} = this.position;
     this.checkOutOfBoundaries();
-    ctx.moveTo(x - 30 * this.size, y - 20 * this.size);
-    ctx.lineTo(x - 10 * this.size, y - 25 * this.size);
-    ctx.lineTo(x - 15 * this.size, y - 17 * this.size);
-    ctx.lineTo(x - 10 * this.size, y - 10 * this.size);
-    ctx.lineTo(x, y - 5 * this.size);
-    ctx.lineTo(x + 5 * this.size, y);
-    ctx.lineTo(x, y + 13 * this.size);
-    ctx.lineTo(x - 5 * this.size, y + 15 * this.size);
-    ctx.lineTo(x - 20 * this.size, y + 13 * this.size);
-    ctx.lineTo(x - 35 * this.size, y - 5 * this.size);
+    ctx.beginPath();
+    ctx.strokeStyle = 'green';
+    ctx.fillStyle = 'lightgreen';
+
+    ctx.moveTo(x - 13 * this.size, y - 5 * this.size);
+    ctx.lineTo(x - 12 * this.size, y - 12 * this.size);
+    ctx.lineTo(x, y - 13 * this.size);
+    ctx.lineTo(x + 10 * this.size, y - 8 * this.size);
+    ctx.lineTo(x + 13 * this.size, y + 3 * this.size);
+    ctx.lineTo(x + 12 * this.size, y + 8 * this.size);
+    ctx.lineTo(x - 8 * this.size, y + 5 * this.size);
+    ctx.lineTo(x - 10 * this.size, y);
+
+    //ctx.lineTo(x - 12 * this.size, y - 12 * this.size);
+
     ctx.closePath();
+
+    ctx.fill();
+
     ctx.stroke();
+
 
   }
 }
@@ -45,10 +54,18 @@ class Bullet extends MovingObject {
 
   draw(ctx) {
     let {x, y} = this.position;
+
+    const collisionCheck = ctx.getImageData(x, y, 1, 1).data[1]; //green pixels
+    if (collisionCheck > 230) {
+      this.hit()
+    };
+
     this.checkOutOfBoundaries();
+    ctx.strokeStyle = 'black';
     ctx.beginPath();
     ctx.arc(x, y, 3, 0, 2 * Math.PI);
     ctx.stroke();
+    ctx.closePath();
   }
 }
 
@@ -56,7 +73,7 @@ class Player extends MovingObject {
 
   constructor(position, speed, accelerationDelta) {
     super(position, speed, accelerationDelta);
-    this.maxSpeed = 5;
+    this.maxSpeed = 8;
     this.shoot = () => {
     };
     this.init();
@@ -99,6 +116,7 @@ class Player extends MovingObject {
     let {x, y} = this.position;
     this.checkOutOfBoundaries();
     ctx.beginPath();
+    ctx.strokeStyle = 'black';
     ctx.moveTo(x - 10, y - 10);
     ctx.lineTo(x + 20, y);
     ctx.lineTo(x - 10, y + 10);
@@ -121,7 +139,6 @@ export default class Game {
     this.width = 0;
     this.center = {x: 0, y: 0};
     this.mouse = {x: 0, y: 0};
-    this.score = 0;
 
     this.init();
   }
@@ -129,10 +146,15 @@ export default class Game {
   init() {
     window.addEventListener("resize", x => this.onResize());
     this.onResize();
+    this.score = 0;
+
     this.player = new Player(this.center, {vx: 0, vy: 0}, 0.7);
     this.player.checkOutOfBoundaries = this.outOfBoundariesWrap(this.player);
-    let asteroids = this.initAsteroids();
-    this.objects = [this.player, ...asteroids];
+    this.asteroidSizes = [1, 3, 4];
+    this.asteroids = this.initAsteroids();
+
+    this.bullets = [];
+    this.objects = [this.player, ...this.asteroids];
     this.canvas.addEventListener('mousemove', (event) => {
       this.mouse = {x: event.offsetX, y: event.offsetY};
     });
@@ -151,6 +173,10 @@ export default class Game {
     this.center = {x: Math.floor(this.width / 2), y: Math.floor(this.height / 2)};
   }
 
+  calcDistance(obj1, obj2) {
+    return Math.sqrt((obj2.x - obj1.x) ** 2 + (obj2.y - obj1.y) ** 2);
+  }
+
   initAsteroids() {
     let arr = [];
     let maxTries = 20;
@@ -162,17 +188,16 @@ export default class Game {
     return arr
   }
 
-  spawnAsteroid() {
+  spawnAsteroid(size, position) {
     let side = Math.floor(Math.random() * 2);
     const boundariesArray = [[0, 0.2 * this.width, 0, this.height], [0.8 * this.width, this.width, 0, this.height]];
     let boundaries = boundariesArray[side];
-    let x = Math.random() * (boundaries[1] - boundaries[0]) + boundaries[0];
-    let y = Math.random() * (boundaries[2] - boundaries[3]) + boundaries[2];
+    let x = position ? position.x : Math.random() * (boundaries[1] - boundaries[0]) + boundaries[0];
+    let y = position ? position.y : Math.random() * (boundaries[2] - boundaries[3]) + boundaries[2];
     let vx = (Math.random() * 5) - 2.5;
     let vy = (Math.random() * 5) - 2.5;
-    let asteroidSizes = [1, 1.5, 2];
-    let size = asteroidSizes[Math.floor(Math.random() * 3)];
-    let asteroid = new Asteroid({x: x, y: y}, {vx: vx, vy: vy}, null, size);
+    let asteroidSize = size ? size : this.asteroidSizes[Math.floor(Math.random() * 3)];
+    let asteroid = new Asteroid({x: x, y: y}, {vx: vx, vy: vy}, null, asteroidSize);
     asteroid.checkOutOfBoundaries = this.outOfBoundariesWrap(asteroid);
     return asteroid;
   }
@@ -180,13 +205,48 @@ export default class Game {
   shoot() {
     const bulletSpeed = 5;
     const vector = {vx: this.mouse.x - this.player.position.x, vy: this.mouse.y - this.player.position.y};
-    const vectorLength = Math.sqrt(vector.vx ** 2 + vector.vy ** 2);
-    const vectorNormalized = {vx: bulletSpeed * (vector.vx / vectorLength), vy: bulletSpeed * (vector.vy / vectorLength)};
+    const vectorLength = this.calcDistance(this.mouse, this.player.position);
+    const vectorNormalized = {
+      vx: bulletSpeed * (vector.vx / vectorLength),
+      vy: bulletSpeed * (vector.vy / vectorLength)
+    };
     let bullet = new Bullet(this.player.position, vectorNormalized);
-    this.objects.push(bullet);
-    bullet.checkOutOfBoundaries = this.outOfBoundariesDestroy(bullet, this.objects.length - 1);
-
+    this.bullets.push(bullet);
+    bullet.checkOutOfBoundaries = this.outOfBoundariesDestroy(bullet, this.bullets.length - 1);
+    bullet.hit = this.bulletHit(bullet);
   };
+
+  bulletHit(bullet) {
+    return () => {
+      let checkDistance = 100;
+      const asteroidSizes = Array.from(this.asteroidSizes);
+      let nearest = null;
+      for (let asteroid of this.asteroids) {
+        const distance = this.calcDistance(asteroid.position, bullet.position);
+        if (checkDistance > distance) {
+          checkDistance = distance;
+          nearest = asteroid;
+        }
+        if (checkDistance < 13) break;
+      }
+      //;
+      this.bullets.splice(this.bullets.indexOf(bullet), 1);
+      if (nearest) { //additional check for simultaneous collisions
+        const asteroidIndex = this.asteroids.indexOf(nearest);
+
+        if (nearest.size > asteroidSizes[0]) {
+          let newSize = asteroidSizes[asteroidSizes.indexOf(nearest.size) - 1];
+
+          this.asteroids.push(this.spawnAsteroid(newSize, nearest.position));
+          this.asteroids.push(this.spawnAsteroid(newSize, nearest.position));
+        } else {
+          this.asteroids.push(this.spawnAsteroid());
+        }
+        this.score += 10 * nearest.size;
+        this.asteroids.splice(asteroidIndex, 1);
+      }
+    }
+  }
 
   outOfBoundariesWrap(object) {
     return () => {
@@ -200,8 +260,8 @@ export default class Game {
   outOfBoundariesDestroy(object) {
     return () => {
       if (object.position.x > this.width || object.position.y > this.height || object.position.x < 0 || object.position.y < 0) {
-        const index = this.objects.indexOf(object);
-        this.objects.splice(index, 1)
+        const index = this.bullets.indexOf(object);
+        this.bullets.splice(index, 1)
       }
     }
   }
@@ -210,6 +270,8 @@ export default class Game {
     const dt = time - this.prevUpdateTime;
     this.prevUpdateTime = time;
     this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.objects = [...this.asteroids, ...this.bullets];
+    this.objects.push(this.player);
     this.objects.forEach((obj) => {
       obj.update();
       obj.draw(this.ctx);
